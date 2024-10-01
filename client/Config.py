bdcad4f6edd51b732173b92b70bdb22f3d095101
Abled-Taha <abledtaha@gmail.com>
2024-10-01 22:40:19 +0500

    Added Config, templates & static files support

---
+++ client/Config.py
@@ -0,0 +1,16 @@
+import json
+
+class Config(object):
+  def __init__(self, BASE_DIR):
+    self.BASE_DIR = BASE_DIR
+  
+  # Loading Config File
+  def readConfig(self):
+    with open(f'{self.BASE_DIR}/../config.json', 'r') as f:
+      self.config = json.load(f)
+      
+      self.server_host = self.config["server_host"]
+      self.server_port = self.config["server_port"]
+      self.secret_key = self.config["secret_key"]
+      self.debug = self.config["debug"]
+      self.allowed_hosts = self.config["allowed_hosts"]
--- client/client/settings.py
+++ client/client/settings.py
@@ -11,21 +11,27 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from Config import Config as ConfigClass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Getting Config
+global ConfigObj
+ConfigObj = ConfigClass(BASE_DIR)
+ConfigObj.readConfig()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#s7(mccb@usp4x*%dne+60q7$o61vtkivsua^a(lx9w8vk!w=w"
+SECRET_KEY = ConfigObj.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ConfigObj.debug
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ConfigObj.allowed_hosts
 
 
 # Application definition
@@ -54,7 +60,7 @@ ROOT_URLCONF = "client.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -73,12 +79,12 @@ WSGI_APPLICATION = "client.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
@@ -115,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static' / 'static_root'
+STATICFILES_DIRS = [BASE_DIR / 'static' / 'static_files']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
