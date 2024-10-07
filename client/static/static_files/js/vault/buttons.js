ed2bcc2593ff1a89917eb76e33253f785be69ac2
Abled-Taha <abledtaha@gmail.com>
2024-10-07 14:42:31 +0500

    Redesigned Vault

--- client/static/static_files/js/vault/buttons.js
+++ client/static/static_files/js/vault/buttons.js
@@ -1,3 +1,35 @@
-document.getElementById("buttonLogout").onclick = function () {
-  location.href = "/logout";
-};
+function openPopup(website, username, password, url, id) {
+  document.getElementById("popup-website").textContent = website;
+  document.getElementById("popup-username").textContent = username;
+  document.getElementById("popup-password").textContent = password;
+  document.getElementById("popup-url").textContent = url;
+  document.getElementById("popup-id").textContent = id;
+  document.getElementById("popup-button").onclick = function() {
+    copyPassword(password)
+  }
+  document.getElementById("popup").style.display = "block";
+  document.addEventListener('keydown', handleKeyDown);
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+  document.removeEventListener('keydown', handleKeyDown);
+}
+
+function copyPassword(password) {
+navigator.clipboard.writeText(password).then(function() {
+    alert("Password copied!");
+}, function(err) {
+    console.error("Could not copy password: ", err);
+});
+}
+
+function deletePassword() {
+  alert("Password deleted!");
+}
+
+function handleKeyDown(event) {
+  if (event.key === "Escape") {
+    closePopup();
+  }
+}
